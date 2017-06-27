8f0f797f56b303d3c688129396431a75ccf98e12
Nikhil Allamsetti <nikhil.allamsetti@gmail.com>
2017-06-28 02:48:04 +0530

    changed everything to pep8

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 import SimpleHTTPServer
 import SocketServer
 
+
 class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-	def do_GET(self):
-		if self.path == '/':
-			self.path = '/response/index.html'
-		return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/response/index.html'
+        return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
 
 Handler = MyRequestHandler
 server = SocketServer.TCPServer(('0.0.0.0', 8080), Handler)
